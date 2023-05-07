c9a72e279516d08f365df099013da57c8de8df00
Sahidmalik001 <106477157+Sahidmalik001@users.noreply.github.com>
2023-05-07 23:11:27 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -25,10 +25,7 @@ class Bot(Client):
         self.mention = me.mention
         self.username = me.username  
         self.uptime = Config.BOT_UPTIME     
-        if Config.WEBHOOK:
-            app = web.AppRunner(await web_server())
-            await app.setup()       
-            await web.TCPSite(app, "0.0.0.0", 8080).start()     
+     
         print(f"{me.first_name} Iꜱ Sᴛᴀʀᴛᴇᴅ.....✨️")
         for id in Config.ADMIN:
             try: await self.send_message(id, f"**__{me.first_name}  Iꜱ Sᴛᴀʀᴛᴇᴅ.....✨️__**")                                
